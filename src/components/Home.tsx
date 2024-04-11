fa61ed19473314348c8b8c3d201699d71f107ab1
Tulza <40633228+tulza@users.noreply.github.com>
2024-04-11 17:37:40 +1200

    center model origin, add text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,13 +4,27 @@ import Speen from "./models/Speen";
 
 const Home = () => {
   return (
-    <Canvas className="bg-red-500">
-      <directionalLight position={[3.3, 1.0, 4.4]} intensity={Math.PI * 2} />
-      <directionalLight position={[-3.3, -1.0, -4.4]} intensity={Math.PI * 2} />
-      <ambientLight />
-      <OrbitControls />
-      <Speen />
-    </Canvas>
+    <div className="grid h-dvh w-dvw place-items-center bg-red-500">
+      <div className="flex w-full justify-around">
+        <span className="text-[10rem] tracking-wide text-white">Coca</span>
+        <span className="text-[10rem] tracking-wide text-white">cola</span>
+      </div>
+      <div className="absolute h-full w-full">
+        <Canvas className="absolute">
+          <directionalLight
+            position={[3.3, 1.0, 4.4]}
+            intensity={Math.PI * 2}
+          />
+          <directionalLight
+            position={[-3.3, -1.0, -4.4]}
+            intensity={Math.PI * 2}
+          />
+          <ambientLight />
+          <OrbitControls enabled={false} />
+          <Speen />
+        </Canvas>
+      </div>
+    </div>
   );
 };
 
--- src/components/models/Speen.tsx
+++ src/components/models/Speen.tsx
@@ -1,19 +1,21 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import Cola from "./Cola";
-
+import * as THREE from "three";
 const Speen = () => {
-  const ref = useRef(null);
+  const ref = useRef<THREE.Group | null>(null);
+
   useFrame(() => {
-    if (!ref) return;
-    //@ts-ignore
-    ref.current.rotation.y += 0.04;
-    //@ts-ignore
-    ref.current.rotation.z += 0.005;
+    if (!ref.current) return;
+    ref.current.rotation.y += 0.01;
+    ref.current.rotation.z = 0.2;
+    ref.current.rotation.x = 0;
   });
   return (
-    <group position={[0, 1, 0]} ref={ref}>
-      <Cola />
+    <group position={[0, 0, 0]} ref={ref}>
+      <group position={[0, -2.5, 0]}>
+        <Cola />
+      </group>
     </group>
   );
 };
